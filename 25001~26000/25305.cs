7da0714adc093559835d127420e28522b445a547
Chunghyun Lim <potterLim0808@gmail.com>
2022-10-11 00:43:36 +0900

    2022-10-11

---
+++ 25001~26000/25305.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countApplicants;
+            int countWinners;
+
+            string[] str = Console.ReadLine().Split(" ");
+            countApplicants = int.Parse(str[0]);
+            countWinners = int.Parse(str[1]);
+
+            string[] scoresStr = Console.ReadLine().Split(" ");
+            int[] scores = new int[countApplicants];
+
+            for (int i = 0; i < countApplicants; ++i)
+            {
+                scores[i] = int.Parse(scoresStr[i]);
+            }
+
+            Array.Sort(scores);
+            Console.WriteLine(scores[scores.Length - countWinners]);
+        }
+    }
+}
