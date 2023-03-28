542fbbd73d4bb7272cdafd83a55177da00580761
Chunghyun Lim <potterLim0808@gmail.com>
2023-03-28 20:04:54 +0900

    2023-03-28

---
+++ 27001~28000/27866.cs
@@ -0,0 +1,12 @@
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(str[n - 1]);
+        }
+    }
+}
---
+++ 7001~8000/7287.cs
@@ -0,0 +1,11 @@
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("199");
+            Console.WriteLine("donerd");
+        }
+    }
+}
