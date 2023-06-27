f2b13429d8eab481886fd3e23c074f965d0955b1
Chunghyun Lim <potterLim0808@gmail.com>
2023-06-28 00:51:20 +0900

    2023-06-28

---
+++ 2001~3000/2193.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int countDigits = int.Parse(Console.ReadLine());
+            long[] fibonacci = new long[countDigits + 1];
+
+            fibonacci[1] = 1;
+            if (countDigits >= 2)
+            {
+                fibonacci[2] = 1;
+            }
+
+            for (int i = 3; i <= countDigits; i++)
+            {
+                fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+            }
+
+            Console.WriteLine(fibonacci[countDigits]);
+        }
+    }
+}
---
+++ 2001~3000/2512.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countProvinces = int.Parse(Console.ReadLine());
+            List<int> budgets = new List<int>(countProvinces);
+
+            string str = Console.ReadLine();
+            string[] splitedStr = str.Split();
+
+            foreach (string budget in splitedStr)
+            {
+                budgets.Add(int.Parse(budget));
+            }
+            budgets.Sort();
+            budgets.Reverse();
+
+            int totalBudget = int.Parse(Console.ReadLine());
+            int maxBudget = budgets[0];
+
+            while (true)
+            {
+                int sumBudget = 0;
+                for (int i = 0; i < countProvinces; i++)
+                {
+                    if (budgets[i] > maxBudget)
+                    {
+                        sumBudget += maxBudget;
+                    }
+                    else
+                    {
+                        sumBudget += budgets[i];
+                    }
+
+                    if (sumBudget > totalBudget)
+                    {
+                        maxBudget--;
+                        break;
+                    }
+                }
+
+                if (sumBudget <= totalBudget)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine(maxBudget);
+        }
+    }
+}
