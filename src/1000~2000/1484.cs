e27d6264134d3be9a53e20002093c589b3baf02a
Chunghyun Lim <potterLim0808@gmail.com>
2023-07-09 08:30:11 +0900

    2023-07-09

---
+++ src/1000~2000/1484.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int targetNumber = int.Parse(Console.ReadLine());
+            long leftNumber = 1;
+            long rightNumber = 1;
+            bool targetFound = false;
+
+            while (true)
+            {
+                if (leftNumber * leftNumber - rightNumber * rightNumber == targetNumber)
+                {
+                    targetFound = true;
+                    Console.WriteLine(leftNumber);
+                }
+
+                if (leftNumber - rightNumber == 1 && leftNumber * leftNumber - rightNumber * rightNumber > targetNumber)
+                {
+                    break;
+                }
+
+                if (leftNumber * leftNumber - rightNumber * rightNumber > targetNumber)
+                {
+                    rightNumber++;
+                }
+                else
+                {
+                    leftNumber++;
+                }
+            }
+
+            if (targetFound == false)
+            {
+                Console.WriteLine(-1);
+            }
+        }
+    }
+}
