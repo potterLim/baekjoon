48e621189796ffb9bfb870401a5ce11c0059ed15
Chunghyun Lim <potterLim0808@gmail.com>
2024-12-29 16:56:28 +0900

    2024-12-29

---
+++ src/18001~19000/18870.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        int count = int.Parse(Console.ReadLine());
+        int[] original = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+        var sortedDistinct = original.Distinct().OrderBy(x => x).ToArray();
+        var rankDictionary = new Dictionary<int, int>();
+
+        for (int i = 0; i < sortedDistinct.Length; i++)
+        {
+            rankDictionary[sortedDistinct[i]] = i;
+        }
+
+        int[] compressed = original.Select(x => rankDictionary[x]).ToArray();
+        Console.WriteLine(string.Join(" ", compressed));
+    }
+}
