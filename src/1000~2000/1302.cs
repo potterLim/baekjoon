7efb8f80449ab91648f8918cd15a694e5ef976d2
Chunghyun Lim <potterLim0808@gmail.com>
2024-12-30 15:51:24 +0900

    2024-12-30

---
+++ src/1000~2000/1302.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int numberOfBooks = int.Parse(Console.ReadLine());
+        Dictionary<string, int> booksSold = new Dictionary<string, int>(numberOfBooks);
+
+        for (int i = 0; i < numberOfBooks; ++i)
+        {
+            string bookName = Console.ReadLine();
+
+            if (booksSold.ContainsKey(bookName))
+            {
+                booksSold[bookName]++;
+            }
+            else
+            {
+                booksSold[bookName] = 1;
+            }
+        }
+
+        var bestSeller = booksSold.OrderByDescending(book => book.Value)
+                                   .ThenBy(book => book.Key)
+                                   .First();
+
+        Console.WriteLine(bestSeller.Key);
+    }
+}
+
