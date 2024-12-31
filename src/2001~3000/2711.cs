544ea390d1c5b57717b726987fa77c7a0cbf3331
Chunghyun Lim <potterLim0808@gmail.com>
2024-12-31 13:55:18 +0900

    2024-12-31

---
+++ src/2001~3000/2711.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int countTestCase = int.Parse(Console.ReadLine());
+        string[] correctedStrings = new string[countTestCase];
+        
+        for (int i = 0; i < countTestCase; ++i)
+        {
+            string[] input = Console.ReadLine().Split();
+            int typoIndex = int.Parse(input[0]) - 1;
+            string originalString = input[1];
+
+            correctedStrings[i] = originalString.Remove(typoIndex, 1);
+        }
+
+        for (int i = 0; i < countTestCase; ++i)
+        {
+            Console.WriteLine(correctedStrings[i]);
+        }
+    }
+}
