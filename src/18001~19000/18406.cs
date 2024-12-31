00285bf2dc3d7c772bf3b111d1dc5cef8280d56e
Chunghyun Lim <potterLim0808@gmail.com>
2024-12-31 13:03:19 +0900

    2024-12-31

---
+++ src/18001~19000/18406.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        string pointStr = Console.ReadLine();
+
+        int sumLeft = 0;
+        int sumRight = 0;
+
+        for (int i = 0; i < pointStr.Length; ++i)
+        {
+            if (i < pointStr.Length / 2)
+            {
+                sumLeft += int.Parse(pointStr[i].ToString());
+            }
+
+            else
+            {
+                sumRight += int.Parse(pointStr[i].ToString());
+            }
+        }
+
+        if (sumLeft == sumRight)
+        {
+            Console.WriteLine("LUCKY");
+        }
+
+        else
+        {
+            Console.WriteLine("READY");
+        }
+    }
+}
