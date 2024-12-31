4976ba3508c9c3f0e68d980317f1c21cacf6bc85
Chunghyun Lim <potterLim0808@gmail.com>
2024-12-31 13:07:34 +0900

    2024-12-31

---
+++ src/10001~11000/10821.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        string numbersStr = Console.ReadLine();
+        int countNumbers = 1;
+
+        for (int i = 0; i < numbersStr.Length; ++i)
+        {
+            if (numbersStr[i] == ',')
+            {
+                countNumbers++;
+            }
+        }
+
+        Console.WriteLine(countNumbers);
+
+    }
+}
