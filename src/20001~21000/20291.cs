6127501660cb546c79e36336f1c38b98c61b76b0
Chunghyun Lim <potterLim0808@gmail.com>
2024-12-31 12:47:11 +0900

    2024-12-31

---
+++ src/20001~21000/20291.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int countFiles = int.Parse(Console.ReadLine());
+        string[] nameFiles = new string[countFiles];
+
+        for (int i = 0; i < countFiles; ++i)
+        {
+            nameFiles[i] = Console.ReadLine();
+            nameFiles[i] = nameFiles[i].Split('.')[1];
+        }
+
+        Dictionary<string, int> countExtensions = new Dictionary<string, int>(countFiles);
+
+        for (int i = 0; i < countFiles; ++i)
+        {
+            if (countExtensions.ContainsKey(nameFiles[i]))
+            {
+                countExtensions[nameFiles[i]]++;
+            }
+
+            else
+            {
+                countExtensions[nameFiles[i]] = 1;
+            }
+        }
+
+        var sortedExtensions = countExtensions.OrderBy(extension => extension.Key);
+
+        foreach (var extensions in sortedExtensions)
+        {
+            Console.WriteLine($"{extensions.Key} {extensions.Value}");
+        }
+    }
+}
