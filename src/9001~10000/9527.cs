afc474718f29b0c9ecbcbfcc6dc78fe2ca9f5062
Chunghyun Lim <potterLim0808@gmail.com>
2025-01-02 01:57:55 +0900

    2025-01-02

---
+++ src/9001~10000/9527.cs
@@ -0,0 +1,33 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+
+        string[] inputs = Console.ReadLine().Split();
+        long A = long.Parse(inputs[0]);
+        long B = long.Parse(inputs[1]);
+
+        Console.WriteLine(CountOnes(B) - CountOnes(A - 1));
+    }
+
+    static long CountOnes(long x)
+    {
+        if (x < 0) return 0;
+
+        long count = 0;
+        long powerOfTwo = 1;
+
+        while (powerOfTwo <= x)
+        {
+            long fullCycles = x / (powerOfTwo * 2);
+            long remainder = x % (powerOfTwo * 2);
+
+            count += fullCycles * powerOfTwo;
+            count += Math.Max(0, remainder - powerOfTwo + 1);
+
+            powerOfTwo *= 2;
+        }
+
+        return count;
+    }
+}
