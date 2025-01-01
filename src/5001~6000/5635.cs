6c0050b6b17680dc78d157749da69e8b87f743c5
Chunghyun Lim <potterLim0808@gmail.com>
2025-01-01 15:25:22 +0900

    2025-01-01

---
+++ src/5001~6000/5635.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace StudentAgeCalculator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int studentCount = int.Parse(Console.ReadLine());
+            List<Student> students = new List<Student>();
+
+            for (int i = 0; i < studentCount; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ');
+                string name = input[0];
+                int day = int.Parse(input[1]);
+                int month = int.Parse(input[2]);
+                int year = int.Parse(input[3]);
+
+                students.Add(new Student(name, new DateTime(year, month, day)));
+            }
+
+            Student youngest = students.OrderByDescending(s => s.Birthday).First();
+            Student oldest = students.OrderBy(s => s.Birthday).First();
+
+            Console.WriteLine(youngest.Name);
+            Console.WriteLine(oldest.Name);
+        }
+    }
+
+    public class Student
+    {
+        public string Name { get; }
+        public DateTime Birthday { get; }
+
+        public Student(string name, DateTime birthday)
+        {
+            Name = name;
+            Birthday = birthday;
+        }
+    }
+}
