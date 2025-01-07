bf0396175200cd095ebc2c82c44013bf9ee5b049
Chunghyun Lim <potterLim0808@gmail.com>
2025-01-08 03:56:04 +0900

    2025-01-08

---
+++ src/4001~5000/4949.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        while (true)
+        {
+            string input = Console.ReadLine();
+
+            if (input == ".")
+            {
+                break;
+            }
+
+            bool isBalanced = IsBalanced(input);
+            if (isBalanced)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+
+    private static bool IsBalanced(string input)
+    {
+        Stack<char> stack = new Stack<char>();
+
+        foreach (char ch in input)
+        {
+            if (ch == '[' || ch == '(')
+            {
+                stack.Push(ch);
+            }
+            else if (ch == ']')
+            {
+                if (stack.Count == 0 || stack.Pop() != '[')
+                {
+                    return false;
+                }
+            }
+            else if (ch == ')')
+            {
+                if (stack.Count == 0 || stack.Pop() != '(')
+                {
+                    return false;
+                }
+            }
+        }
+
+        if (stack.Count == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
