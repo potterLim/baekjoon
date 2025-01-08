a22bc9776ec4e9a8eb9c90979142f8d6f8f5588b
Chunghyun Lim <potterLim0808@gmail.com>
2025-01-09 05:19:23 +0900

    2025-01-09

---
+++ src/10001~11000/10822.cs
@@ -0,0 +1,15 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        string[] strNums = Console.ReadLine().Split(',');
+        int sum = 0;
+
+        for (int i = 0; i < strNums.Length; ++i)
+        {
+            sum += int.Parse(strNums[i]);
+        }
+
+        Console.WriteLine(sum);
+    }
+}
