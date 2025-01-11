f2f2f97ce84d0a3846d47e6a53197da32d3e6464
Chunghyun Lim <potterLim0808@gmail.com>
2025-01-12 01:09:46 +0900

    [C#] 알파벳 거리

---
+++ src/5001~6000/5218.cs
@@ -0,0 +1,33 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        int countStr = int.Parse(Console.ReadLine());
+        for (int i = 0; i < countStr; ++i)
+        {
+            string[] separatedStr = Console.ReadLine().Split(' ');
+            Console.Write("Distances: ");
+            for (int j = 0; j < separatedStr[0].Length; ++j)
+            {
+                if (separatedStr[0][j] <= separatedStr[1][j])
+                {
+                    Console.Write($"{separatedStr[1][j] - separatedStr[0][j]}");
+                    if (j != separatedStr[0].Length - 1)
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                else
+                {
+                    Console.Write($"{separatedStr[1][j] + 26 - separatedStr[0][j]}");
+                    if (j != separatedStr[0].Length - 1)
+                    {
+                        Console.Write(" ");
+                    }
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
