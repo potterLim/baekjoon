7c1a866cdb55a39504c234a9682e39ca9d0e6eb9
Chunghyun Lim <potterLim0808@gmail.com>
2025-01-12 01:25:52 +0900

    [C#] 등장하지 않는 문자의 합

---
+++ src/3001~4000/3059.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        const int ASCII_OFFSET = 65;
+        const int ALPHABET_COUNT = 26;
+
+        int testCaseCount = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < testCaseCount; ++i)
+        {
+            string inputString = Console.ReadLine();
+            bool[] existAlphabet = new bool[ALPHABET_COUNT];
+            int sum = 0;
+
+            foreach (char c in inputString)
+            {
+                existAlphabet[c - ASCII_OFFSET] = true;
+            }
+
+            for (int j = 0; j < ALPHABET_COUNT; ++j)
+            {
+                if (existAlphabet[j] != true)
+                {
+                    sum += j + ASCII_OFFSET;
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
