59af45cb513433b9cdd4679d35f29d0eae5d6cb8
Chunghyun Lim <potterLim0808@gmail.com>
2025-01-13 06:00:18 +0900

    [C#] 파일 완전 삭제

---
+++ src/9001~10000/9243.cs
@@ -0,0 +1,41 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        int countReverse = int.Parse(Console.ReadLine());
+        string originalBits = Console.ReadLine();
+        string modifiedBits = Console.ReadLine();
+
+        bool isDeletionSucceeded;
+
+        if (countReverse % 2 == 0)
+        {
+            isDeletionSucceeded = originalBits == modifiedBits;
+        }
+        else
+        {
+            isDeletionSucceeded = isFlipped(originalBits, modifiedBits);
+        }
+
+        if (isDeletionSucceeded)
+        {
+            Console.WriteLine("Deletion succeeded");
+        }
+        else
+        {
+            Console.WriteLine("Deletion failed");
+        }
+    }
+
+    private static bool isFlipped(string originalBits, string modifiedBits)
+    {
+        for (int i = 0; i < originalBits.Length; i++)
+        {
+            if (originalBits[i] == modifiedBits[i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
