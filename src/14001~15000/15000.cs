d1fa1c21abe22b1f04996b9f943756f2a374d865
Chunghyun Lim <potterLim0808@gmail.com>
2025-01-16 01:44:29 +0900

    [C#] CAPS

---
+++ src/14001~15000/15000.cs
@@ -0,0 +1,9 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        string str = Console.ReadLine();
+        str = str.ToUpper();
+        Console.WriteLine(str);
+    }
+}
