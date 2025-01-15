8105021e6aa93ab042b2a9056b02516314fc8421
Chunghyun Lim <potterLim0808@gmail.com>
2025-01-16 02:55:32 +0900

    [C#] JOI와 IOI

---
+++ src/5001~6000/5586.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        int countJOI = 0;
+        int countIOI = 0;
+
+        for (int i = 0; i < input.Length - 2; ++i)
+        {
+            if (input[i] == 'J' && input[i + 1] == 'O' && input[i + 2] == 'I')
+            {
+                countJOI++;
+            }
+            else if (input[i] == 'I' && input[i + 1] == 'O' && input[i + 2] == 'I')
+            {
+                countIOI++;
+            }
+        }
+
+        Console.WriteLine(countJOI);
+        Console.WriteLine(countIOI);
+    }
+}
