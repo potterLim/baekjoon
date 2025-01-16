9fcd57b381fdfed75775e61e8215f43509b18dbd
Chunghyun Lim <potterLim0808@gmail.com>
2025-01-17 06:43:35 +0900

    [C#] 줄번호

---
+++ src/4001~5000/4470.cs
@@ -0,0 +1,13 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        int countStr = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < countStr; ++i)
+        {
+            string input = Console.ReadLine();
+            Console.WriteLine($"{i+1}. {input}");
+        }
+    }
+}
