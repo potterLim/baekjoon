7eddf4442117b034ee40f15894d09fa4860eebca
Chunghyun Lim <potterLim0808@gmail.com>
2025-01-17 03:21:46 +0900

    [C#] 줄 세우기

---
+++ src/11001~12000/11536.cs
@@ -0,0 +1,29 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        int countPeople = int.Parse(Console.ReadLine());
+        List<string> NamesPeople = new List<string>(countPeople);
+
+        for (int i = 0; i < countPeople; ++i)
+        {
+            NamesPeople.Add(Console.ReadLine());
+        }
+
+        bool isAscending = NamesPeople.SequenceEqual(NamesPeople.OrderBy(name => name));
+        bool isDescending = NamesPeople.SequenceEqual(NamesPeople.OrderByDescending(name => name));
+
+        if (isAscending)
+        {
+            Console.WriteLine("INCREASING");
+        }
+        else if (isDescending)
+        {
+            Console.WriteLine("DECREASING");
+        }
+        else
+        {
+            Console.WriteLine("NEITHER");
+        }
+    }
+}
