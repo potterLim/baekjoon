a4b1492fa5decc72ace5af0e0ada7f2dded97e1e
Chunghyun Lim <potterLim0808@gmail.com>
2025-01-17 06:37:31 +0900

    [C#] A + B - C

---
+++ src/31001~32000/31403.cs
@@ -0,0 +1,12 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        string A = Console.ReadLine();
+        string B = Console.ReadLine();
+        string C = Console.ReadLine();
+
+        Console.WriteLine(int.Parse(A) + int.Parse(B) - int.Parse(C));
+        Console.WriteLine(int.Parse(A + B) - int.Parse(C));
+    }
+}
