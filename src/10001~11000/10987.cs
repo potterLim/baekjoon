38e01790d6f72a9927018cc7b69dcfdb6bd20886
Chunghyun Lim <potterLim0808@gmail.com>
2025-01-17 06:40:55 +0900

    [C#] 모음의 개수

---
+++ src/10001~11000/10987.cs
@@ -0,0 +1,18 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        int countVowels = 0;
+
+        for (int i = 0; i < input.Length; ++i)
+        {
+            if (input[i] == 'a' || input[i] == 'e' || input[i] == 'i' || input[i] == 'o' || input[i] == 'u')
+            {
+                countVowels++;
+            }
+        }
+
+        Console.WriteLine(countVowels);
+    }
+}
