bdd5fbfc20bef61da70c6018f71a19d61b66e4f3
Chunghyun Lim <potterLim0808@gmail.com>
2025-01-19 05:10:51 +0900

    [C#] 도비의 영어 공부

---
+++ src/2001~3000/2386.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        while (true)
+        {
+            string input = Console.ReadLine();
+            if (input == "#")
+            {
+                break;
+            }
+
+
+            char targetChar = input[0];
+            string sentence = input.Substring(2);
+
+            targetChar = char.ToLower(targetChar);
+            sentence = sentence.ToLower();
+
+            int count = 0;
+            for (int i = 0; i < sentence.Length; ++i)
+            {
+                if (sentence[i] == targetChar)
+                {
+                    count++;
+                }
+            }
+
+
+            Console.WriteLine($"{targetChar} {count}");
+        }
+    }
+}
