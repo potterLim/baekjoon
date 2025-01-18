003d48dbd9f31109e5a81829bffbfef0d460111d
Chunghyun Lim <potterLim0808@gmail.com>
2025-01-19 05:05:36 +0900

    [C#] 정보보호학부 동아리 소개

---
+++ src/28001~29000/28691.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+
+        if (input == "M")
+        {
+            Console.WriteLine("MatKor");
+        } 
+        else if (input == "W")
+        {
+            Console.WriteLine("WiCys");
+        }
+
+        else if (input == "C")
+        {
+            Console.WriteLine("CyKor");
+        }
+
+        else if (input == "A")
+        {
+            Console.WriteLine("AlKor");
+        }
+
+        else if (input == "$")
+        {
+            Console.WriteLine("$clear");
+        }
+
+    }
+}
