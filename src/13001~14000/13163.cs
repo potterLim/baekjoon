7f9cce6b65b2e77be45a3076a1852a6a4401b0fa
Chunghyun Lim <potterLim0808@gmail.com>
2025-01-20 07:26:02 +0900

    [C#] 닉네임에 갓 붙이기

---
+++ src/13001~14000/13163.cs
@@ -0,0 +1,17 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        int count = int.Parse(Console.ReadLine());
+        for (int i = 0; i < count; ++i)
+        {
+            string[] inputStr = Console.ReadLine().Split(" ");
+            Console.Write("god");
+            for (int j = 1; j < inputStr.Length; ++j)
+            {
+                Console.Write(inputStr[j]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
