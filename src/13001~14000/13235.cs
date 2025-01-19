85c85631e8b47981dcca12b9ec13e50ef71a0c4d
Chunghyun Lim <potterLim0808@gmail.com>
2025-01-20 07:33:37 +0900

    [C#] 팰린드롬

---
+++ src/13001~14000/13235.cs
@@ -0,0 +1,19 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        bool isPalindrome = true;
+
+        for (int i = 0; i < (input.Length - 1) / 2; ++i)
+        {
+            if (input[i] != input[input.Length - 1 - i])
+            {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        Console.WriteLine(isPalindrome ? "true" : "false");
+    }
+}
