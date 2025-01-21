fb338ef14125ec5f28ef2b44aa1cb07d38d1ad25
Chunghyun Lim <potterLim0808@gmail.com>
2025-01-21 11:27:38 +0900

    [C#] 공백왕 빈-칸

---
+++ src/3001~4000/3518.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<string> lines = new List<string>();
+        string inputLine;
+
+        while ((inputLine = Console.ReadLine()) != null)
+        {
+            lines.Add(inputLine.Trim());
+        }
+
+        List<string[]> wordsByLine = lines.Select(line => line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)).ToList();
+
+        int maxColumns = wordsByLine.Max(words => words.Length);
+        int[] maxColumnWidths = new int[maxColumns];
+
+        foreach (var words in wordsByLine)
+        {
+            for (int i = 0; i < words.Length; i++)
+            {
+                maxColumnWidths[i] = Math.Max(maxColumnWidths[i], words[i].Length);
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+
+        foreach (var words in wordsByLine)
+        {
+            for (int i = 0; i < words.Length; i++)
+            {
+                result.Append(words[i]);
+
+                if (i < words.Length - 1)
+                {
+                    int padding = maxColumnWidths[i] - words[i].Length + 1;
+                    result.Append(' ', padding);
+                }
+            }
+
+            result.AppendLine();
+        }
+
+        Console.Write(result.ToString());
+    }
+}
