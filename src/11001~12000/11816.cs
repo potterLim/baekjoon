c34729295472c6edcd056116e7aee54df79c4cfc
Chunghyun Lim <potterLim0808@gmail.com>
2025-01-22 11:38:02 +0900

    [C#] 8진수, 10진수, 16진수

---
+++ src/11001~12000/11816.cs
@@ -0,0 +1,24 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+
+        int decimalValue;
+
+        if (input.StartsWith("0x"))
+        {
+            decimalValue = Convert.ToInt32(input, 16);
+        }
+        else if (input.StartsWith("0"))
+        {
+            decimalValue = Convert.ToInt32(input, 8);
+        }
+        else
+        {
+            decimalValue = int.Parse(input);
+        }
+
+        Console.WriteLine(decimalValue);
+    }
+}
