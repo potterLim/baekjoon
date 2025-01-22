aca0575dcfa675a1ef6972b347524ac43a0d7132
Chunghyun Lim <potterLim0808@gmail.com>
2025-01-22 11:40:25 +0900

    [C#] MBTI

---
+++ src/25001~26000/25640.cs
@@ -0,0 +1,19 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        string userMbti = Console.ReadLine();
+        int friendCount = int.Parse(Console.ReadLine());
+        int matchingCount = 0;
+
+        for (int i = 0; i < friendCount; i++)
+        {
+            if (Console.ReadLine() == userMbti)
+            {
+                matchingCount++;
+            }
+        }
+
+        Console.WriteLine(matchingCount);
+    }
+}
