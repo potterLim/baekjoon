e4f2566897aa8d82c3ce93d7c995fe93ad3640e5
Chunghyun Lim <potterLim0808@gmail.com>
2025-01-22 11:34:31 +0900

    [C#] FBI

---
+++ src/2001~3000/2857.cs
@@ -0,0 +1,35 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        bool[] isFBI = new bool[5];
+        bool existFBI = false;
+        for (int i = 0; i < 5; ++i)
+        {
+            string input = Console.ReadLine();
+            for (int j = 0; j < input.Length - 2; ++j)
+            {
+                if (input.Substring(j, 3) == "FBI")
+                {
+                    isFBI[i] = true;
+                    existFBI = true;
+                }
+            }
+        }
+
+        if (existFBI)
+        {
+            for (int i = 0; i < 5; ++i)
+            {
+                if (isFBI[i])
+                {
+                    Console.Write($"{i + 1} ");
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine("HE GOT AWAY!");
+        }
+    }
+}
