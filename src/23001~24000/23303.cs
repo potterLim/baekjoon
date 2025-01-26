6e22781e787b48e464b8e8a88fe495ff601ae915
Chunghyun Lim <potterLim0808@gmail.com>
2025-01-27 01:56:10 +0900

    [C#] 이 문제는 D2 입니다.

---
+++ src/23001~24000/23303.cs
@@ -0,0 +1,22 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine().ToLower();
+        int i;
+
+        for (i = 0; i < input.Length - 1; ++i)
+        {
+            if (input.Substring(i, 2) == "d2")
+            {
+                Console.WriteLine("D2");
+                break;
+            }
+        }
+
+        if (i == input.Length - 1)
+        {
+            Console.WriteLine("unrated");
+        }
+    }
+}
