dbeab550e576be4a55404e9c6f53f32bbd861e39
Chunghyun Lim <potterLim0808@gmail.com>
2025-01-27 01:52:16 +0900

    [C#] 알파벳 전부 쓰기

---
+++ src/11001~12000/11091.cs
@@ -0,0 +1,47 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        int count = int.Parse(Console.ReadLine());
+        for (int i = 0; i < count; ++i)
+        {
+            bool[] existAlpha = new bool[26];
+            string input = Console.ReadLine().ToLower();
+            bool bAllTrue = true;
+
+            for (int j = 0; j < input.Length; ++j)
+            {
+                if (char.IsLetter(input[j]))
+                {
+                    existAlpha[input[j] - 'a'] = true;
+                }
+            }
+
+            for (int j = 0; j < 26; ++j)
+            {
+                if (existAlpha[j] == false)
+                {
+                    bAllTrue = false;
+                    break;
+                }
+            }
+
+            if (bAllTrue)
+            {
+                Console.WriteLine("pangram");
+            }
+            else
+            {
+                Console.Write("missing ");
+                for (int j = 0; j < 26; ++j)
+                {
+                    if (existAlpha[j] == false)
+                    {
+                        Console.Write($"{(char)(j + 'a')}");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
