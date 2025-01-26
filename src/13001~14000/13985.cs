a33c2516a8cb48bd943a4d6e160537edd1f60cea
Chunghyun Lim <potterLim0808@gmail.com>
2025-01-27 01:38:28 +0900

    [C#] Equality

---
+++ src/13001~14000/13985.cs
@@ -0,0 +1,19 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        int num1 = int.Parse(input[0].ToString());
+        int num2 = int.Parse(input[4].ToString());
+        int sum = int.Parse(input[8].ToString());
+
+        if (num1 + num2 == sum)
+        {
+            Console.WriteLine("YES");
+        }
+        else
+        {
+            Console.WriteLine("NO");
+        }
+    }
+}
