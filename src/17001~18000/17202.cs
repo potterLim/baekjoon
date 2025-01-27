8400d0b74cb5c81360655c1c790a148a27572de1
Chunghyun Lim <potterLim0808@gmail.com>
2025-01-28 00:13:54 +0900

    [C#] 핸드폰 번호 궁합

---
+++ src/17001~18000/17202.cs
@@ -0,0 +1,31 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        string phoneNumberA = Console.ReadLine();
+        string phoneNumberB = Console.ReadLine();
+
+        List<int> combinedNumber = new List<int>();
+
+        for (int i = 0; i < phoneNumberA.Length; i++)
+        {
+            combinedNumber.Add(phoneNumberA[i] - '0');
+            combinedNumber.Add(phoneNumberB[i] - '0');
+        }
+
+        while (combinedNumber.Count > 2)
+        {
+            List<int> nextNumbers = new List<int>();
+
+            for (int i = 0; i < combinedNumber.Count - 1; i++)
+            {
+                int sum = combinedNumber[i] + combinedNumber[i + 1];
+                nextNumbers.Add(sum % 10);
+            }
+
+            combinedNumber = nextNumbers;
+        }
+
+        Console.WriteLine($"{combinedNumber[0]}{combinedNumber[1]}");
+    }
+}
