d968a8d7e036b1e5e83069e6654da2037b3c1b4c
Chunghyun Lim <potterLim0808@gmail.com>
2025-01-27 18:13:31 +0900

    [C#] 접미사 배열

---
+++ src/11001~12000/11656.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        string[] suffixes = new string[input.Length];
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            suffixes[i] = input.Substring(i);
+        }
+
+        Array.Sort(suffixes);
+
+        foreach (string suffix in suffixes)
+        {
+            Console.WriteLine(suffix);
+        }
+    }
+}
