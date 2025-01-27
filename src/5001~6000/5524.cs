8d5fa5d46d6a10e388fd8a99473513cf576ac0ea
Chunghyun Lim <potterLim0808@gmail.com>
2025-01-28 00:01:12 +0900

    [C#] 입실 관리

---
+++ src/5001~6000/5524.cs
@@ -0,0 +1,13 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        int count = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < count; ++i)
+        {
+            string input = Console.ReadLine();
+            Console.WriteLine(input.ToLower());
+        }
+    }
+}
