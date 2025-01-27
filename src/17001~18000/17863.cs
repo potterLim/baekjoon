5ba9da3cbc7a3e52eeea123fbf9eb1ef38a55970
Chunghyun Lim <potterLim0808@gmail.com>
2025-01-28 00:05:08 +0900

    [C#] FYI

---
+++ src/17001~18000/17863.cs
@@ -0,0 +1,16 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+
+        if (input.Substring(0, 3) == "555")
+        {
+            Console.WriteLine("YES");
+        }
+        else
+        {
+            Console.WriteLine("NO");
+        }
+    }
+}
