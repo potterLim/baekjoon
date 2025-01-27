39fddd05de4ef7dd62380124a614e3a61389cb88
Chunghyun Lim <potterLim0808@gmail.com>
2025-01-28 00:07:13 +0900

    [C#] 母音を数える (Counting Vowels)

---
+++ src/18001~19000/18409.cs
@@ -0,0 +1,20 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        int length = int.Parse(Console.ReadLine());
+        string input = Console.ReadLine();
+
+        int countVowels = 0;
+
+        for (int i = 0; i < length; ++i)
+        {
+            if (input[i] == 'a' || input[i] == 'e' || input[i] == 'i' || input[i] == 'o' || input[i] == 'u')
+            {
+                countVowels++;
+            }
+        }
+
+        Console.WriteLine(countVowels);
+    }
+}
