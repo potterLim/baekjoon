394e642a510006de94844fb513b2c6b572ea155b
Chunghyun Lim <potterLim0808@gmail.com>
2025-01-29 00:37:53 +0900

    [C#] 사이클 단어

---
+++ src/1000~2000/1544.cs
@@ -0,0 +1,35 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        HashSet<string> uniqueWords = new HashSet<string>();
+
+        for (int i = 0; i < n; i++)
+        {
+            string word = Console.ReadLine();
+            if (!isCyclicWordInSet(word, uniqueWords))
+            {
+                uniqueWords.Add(word);
+            }
+        }
+
+        Console.WriteLine(uniqueWords.Count);
+    }
+
+    private static bool isCyclicWordInSet(string word, HashSet<string> wordSet)
+    {
+        int length = word.Length;
+
+        for (int i = 0; i < length; i++)
+        {
+            string rotatedWord = word.Substring(i) + word.Substring(0, i);
+            if (wordSet.Contains(rotatedWord))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
