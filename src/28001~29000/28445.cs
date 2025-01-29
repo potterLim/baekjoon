d1e908175b64ae4ebc68dd84e0ab25dd7bb44374
potterLim <potterLim0808@gmail.com>
2025-01-30 02:10:19 +0900

    [C#] 알록달록 앵무새

---
+++ src/28001~29000/28445.cs
@@ -0,0 +1,44 @@
+﻿public class Program
+{
+    private static void Main()
+    {
+        string[] fatherColors = Console.ReadLine().Split(' ');
+        string[] motherColors = Console.ReadLine().Split(' ');
+
+        HashSet<string> bodyColors = new HashSet<string>();
+        HashSet<string> tailColors = new HashSet<string>();
+
+        foreach (string color in fatherColors)
+        {
+            bodyColors.Add(color);
+            tailColors.Add(color);
+        }
+
+        foreach (string color in motherColors)
+        {
+            bodyColors.Add(color);
+            tailColors.Add(color);
+        }
+
+        List<string> sortedBodyColors = bodyColors.ToList();
+        List<string> sortedTailColors = tailColors.ToList();
+
+        sortedBodyColors.Sort();
+        sortedTailColors.Sort();
+
+        List<string> results = new List<string>();
+
+        foreach (string body in sortedBodyColors)
+        {
+            foreach (string tail in sortedTailColors)
+            {
+                results.Add(body + " " + tail);
+            }
+        }
+
+        foreach (string result in results)
+        {
+            Console.WriteLine(result);
+        }
+    }
+}
