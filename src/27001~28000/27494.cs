e1f1c5c45535d445c9a1465bcb00d2aafae19984
potterLim <potterLim0808@gmail.com>
2025-01-30 02:21:23 +0900

    [C#] 2023년은 검은 토끼의 해

---
+++ src/27001~28000/27494.cs
@@ -0,0 +1,47 @@
+﻿
+public class Program
+{
+    private const string TARGET = "2023";
+
+    public static void Main()
+    {
+        int ticketCount = int.Parse(Console.ReadLine()!);
+        int winningCount = countWinningTickets(ticketCount);
+        Console.WriteLine(winningCount);
+    }
+
+    private static int countWinningTickets(int ticketCount)
+    {
+        int count = 0;
+
+        for (int i = 2023; i <= ticketCount; i++)
+        {
+            if (containsTargetSequence(i.ToString()))
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    private static bool containsTargetSequence(string serial)
+    {
+        int index = 0;
+
+        foreach (char digit in serial)
+        {
+            if (digit == TARGET[index])
+            {
+                index++;
+
+                if (index == TARGET.Length)
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
