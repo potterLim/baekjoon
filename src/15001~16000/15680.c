2091d698793e8ba059117946e855e63dc4adb787
potterLim <potterLim0808@gmail.com>
2025-02-03 03:37:22 +0900

    [C89] 연세대학교

---
+++ src/15001~16000/15680.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    scanf("%d", &n);
+
+    if (n == 0) {
+        printf("YONSEI\n");
+    } else {
+        printf("Leading the Way to the Future\n");
+    }
+    
+    return 0;
+}
