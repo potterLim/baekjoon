e1469430612baf61d7d59a402fc49433e55fc704
potterLim <potterLim0808@gmail.com>
2025-02-03 03:50:34 +0900

    [C#] Yangjojang of The Year

---
+++ src/11001~12000/11557.cs
@@ -0,0 +1,29 @@
+﻿public class Program
+{
+    private static void Main()
+    {
+        int testCaseCount = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < testCaseCount; ++i)
+        {
+            int schoolCount = int.Parse(Console.ReadLine());
+            string maxAlcoholSchool = string.Empty;
+            int maxAlcohol = 0;
+
+            for (int j = 0; j < schoolCount; ++j)
+            {
+                string[] input = Console.ReadLine().Split(' ');
+                string schoolName = input[0];
+                int alcoholAmount = int.Parse(input[1]);
+
+                if (alcoholAmount > maxAlcohol)
+                {
+                    maxAlcohol = alcoholAmount;
+                    maxAlcoholSchool = schoolName;
+                }
+            }
+
+            Console.WriteLine(maxAlcoholSchool);
+        }
+    }
+}
