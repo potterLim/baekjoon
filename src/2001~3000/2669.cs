efdb34a67f938a9e07276063b2d4863efb400764
potterLim <potterLim0808@gmail.com>
2025-02-04 02:36:02 +0900

    [C#] 직사각형 네개의 합집합의 면적 구하기

---
+++ src/2001~3000/2669.cs
@@ -0,0 +1,27 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int[] scores = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+        int maxScore = scores[0];
+        int minScore = scores[0];
+
+        for (int i = 1; i < n; ++i)
+        {
+            if (scores[i] > maxScore)
+            {
+                maxScore = scores[i];
+            }
+
+            if (scores[i] < minScore)
+            {
+                minScore = scores[i];
+            }
+        }
+
+        Console.WriteLine(maxScore - minScore);
+    }
+}
