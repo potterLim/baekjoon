7686a90ac336aa41f5c6b033dcd09ac769804fc1
potterLim <potterLim0808@gmail.com>
2025-02-26 05:09:00 +0900

    [C#] 4와 7

---
+++ src/2001~3000/2877.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int k = int.Parse(Console.ReadLine());
+            int total = 0;
+            int L = 0;
+            while (total < k)
+            {
+                L++;
+                total += (1 << L);
+            }
+
+            int prevTotal = total - (1 << L);
+            int index = k - prevTotal - 1;
+            StringBuilder sb = new StringBuilder();
+            for (int i = L - 1; i >= 0; i--)
+            {
+                sb.Append(((index & (1L << i)) != 0) ? '7' : '4');
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
