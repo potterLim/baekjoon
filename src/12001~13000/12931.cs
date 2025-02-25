f7a2206962c0cae8efc485f1d119e8329dce4a5c
potterLim <potterLim0808@gmail.com>
2025-02-26 05:13:41 +0900

    [C#] 두 배 더하기

---
+++ src/12001~13000/12931.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int arraySize = int.Parse(Console.ReadLine());
+            int[] targetArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int incrementOperations = 0;
+            int maxDoubleOperations = 0;
+
+            foreach (int number in targetArray)
+            {
+                int value = number;
+                int bitCount = 0;
+                while (value > 0)
+                {
+                    if ((value & 1) == 1)
+                    {
+                        incrementOperations++;
+                    }
+                    bitCount++;
+                    value /= 2;
+                }
+                maxDoubleOperations = Math.Max(maxDoubleOperations, bitCount > 0 ? bitCount - 1 : 0);
+            }
+
+            Console.WriteLine(incrementOperations + maxDoubleOperations);
+        }
+    }
+}
