2177787216badfc9e30c7f20bd546a8304b69f3c
potterLim <potterLim0808@gmail.com>
2025-05-13 01:16:27 +0900

    [C#] 문자열

---
+++ src/1000~2000/1120.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace StringDifference
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string[] inputs = Console.ReadLine().Split();
+            string str1 = inputs[0];
+            string str2 = inputs[1];
+
+            int minDifference = int.MaxValue;
+
+            for (int i = 0; i <= str2.Length - str1.Length; ++i)
+            {
+                int difference = 0;
+
+                for (int j = 0; j < str1.Length; ++j)
+                {
+                    if (str1[j] != str2[i + j])
+                    {
+                        ++difference;
+                    }
+                }
+
+                if (difference < minDifference)
+                {
+                    minDifference = difference;
+                }
+            }
+
+            Console.WriteLine(minDifference);
+        }
+    }
+}
