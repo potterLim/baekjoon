eb4f1a408cb86dce676f16aaad0be002b74b5d6e
potterLim <potterLim0808@gmail.com>
2025-05-13 01:22:07 +0900

    [C#] 시리얼 번호

---
+++ src/1000~2000/1431.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GuitarSerialSorter
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> serials = new List<string>();
+
+            for (int i = 0; i < n; ++i)
+            {
+                serials.Add(Console.ReadLine());
+            }
+
+            serials.Sort((a, b) =>
+            {
+                if (a.Length != b.Length)
+                {
+                    return a.Length - b.Length;
+                }
+
+                int aSum = getDigitSum(a);
+                int bSum = getDigitSum(b);
+
+                if (aSum != bSum)
+                {
+                    return aSum - bSum;
+                }
+
+                return string.Compare(a, b, StringComparison.Ordinal);
+            });
+
+            foreach (string serial in serials)
+            {
+                Console.WriteLine(serial);
+            }
+        }
+
+        private static int getDigitSum(string serial)
+        {
+            int sum = 0;
+
+            foreach (char ch in serial)
+            {
+                if (char.IsDigit(ch))
+                {
+                    sum += ch - '0';
+                }
+            }
+
+            return sum;
+        }
+    }
+}
