f0fe0397c21190f8acc009c14abec59aa7ff5d51
potterLim <potterLim0808@gmail.com>
2025-08-11 01:29:08 +0900

    [Java] 알고리즘 수업 - 알고리즘의 수행 시간 1

---
+++ src/24001~25000/24262.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        new BufferedReader(new InputStreamReader(System.in)).readLine();
+        System.out.println(1);
+        System.out.println(0);
+    }
+}
