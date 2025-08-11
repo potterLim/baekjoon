a6747d3f97765f2d2e12f310d52f164027bdf2e2
potterLim <potterLim0808@gmail.com>
2025-08-11 23:12:33 +0900

    [Java] 수학은 비대면강의입니다

---
+++ src/19001~20000/19532.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+        int f = Integer.parseInt(st.nextToken());
+
+        int det = a * e - b * d;
+        int x = (c * e - b * f) / det;
+        int y = (a * f - c * d) / det;
+
+        System.out.println(x + " " + y);
+    }
+}
