df6e6554331cc1aa0e28733efd89d8be8f3fd874
potterLim <potterLim0808@gmail.com>
2025-08-12 08:21:44 +0900

    [Java] 좌표 정렬하기

---
+++ src/11001~12000/11650.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+        int pointCount = Integer.parseInt(tokenizer.nextToken());
+
+        int[][] points = new int[pointCount][2];
+
+        for (int i = 0; i < pointCount; i++) {
+            tokenizer = new StringTokenizer(reader.readLine());
+            int x = Integer.parseInt(tokenizer.nextToken());
+            int y = Integer.parseInt(tokenizer.nextToken());
+            points[i][0] = x;
+            points[i][1] = y;
+        }
+
+        Arrays.sort(points, (a, b) -> {
+            if (a[0] != b[0]) {
+                return Integer.compare(a[0], b[0]);
+            } else {
+                return Integer.compare(a[1], b[1]);
+            }
+        });
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < pointCount; i++) {
+            sb.append(points[i][0]).append(' ').append(points[i][1]).append('\n');
+        }
+
+        System.out.print(sb.toString());
+    }
+}
