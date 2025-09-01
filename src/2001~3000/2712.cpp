136ec5af9a20b112fe98d464c4ba8d8deb00c3c4
potterLim <potterLim0808@gmail.com>
2025-09-02 00:47:36 +0900

    [C++] 미국 스타일

---
+++ src/2001~3000/2712.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+int main()
+{
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+
+	int testCount;
+	std::cin >> testCount;
+
+	for (int i = 0; i < testCount; ++i)
+	{
+		double value;
+		std::string unit;
+		std::cin >> value >> unit;
+
+		double resultValue;
+		std::string resultUnit;
+
+		if (unit == "kg")
+		{
+			resultValue = value * 2.2046;
+			resultUnit = "lb";
+		}
+		else if (unit == "lb")
+		{
+			resultValue = value * 0.4536;
+			resultUnit = "kg";
+		}
+		else if (unit == "l")
+		{
+			resultValue = value * 0.2642;
+			resultUnit = "g";
+		}
+		else if (unit == "g")
+		{
+			resultValue = value * 3.7854;
+			resultUnit = "l";
+		}
+
+		std::cout << std::fixed << std::setprecision(4) << resultValue << ' ' << resultUnit << std::endl;
+	}
+
+	return 0;
+}
