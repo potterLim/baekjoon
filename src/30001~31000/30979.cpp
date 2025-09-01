df10d3a31c25ba1e8d27f03b8712d01491b5d33b
potterLim <potterLim0808@gmail.com>
2025-09-02 00:49:04 +0900

    [C++] 유치원생 파댕이 돌보기

---
+++ src/30001~31000/30979.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+
+	int totalMinutes;
+	int candyCount;
+
+	std::cin >> totalMinutes;
+	std::cin >> candyCount;
+
+	std::vector<int> flavors;
+	flavors.resize(static_cast<std::size_t>(candyCount));
+
+	int summedMinutes = 0;
+	for (int i = 0; i < candyCount; ++i)
+	{
+		std::cin >> flavors[static_cast<std::size_t>(i)];
+		summedMinutes = summedMinutes + flavors[static_cast<std::size_t>(i)];
+	}
+
+	if (summedMinutes >= totalMinutes)
+	{
+		std::cout << "Padaeng_i Happy\n";
+	}
+	else
+	{
+		std::cout << "Padaeng_i Cry\n";
+	}
+
+	return 0;
+}
