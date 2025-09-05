535d070e68a8d92afb04cfc074cc9db0159283df
potterLim <potterLim0808@gmail.com>
2025-09-05 23:30:47 +0900

    [C++] 바이러스

--- src/2001~3000/2606.cpp
+++ src/2001~3000/2606.cpp
@@ -4,50 +4,50 @@
 
 int main()
 {
-	int nodeCount;
-	std::cin >> nodeCount;
+    int nodeCount;
+    std::cin >> nodeCount;
 
-	int pairCount;
-	std::cin >> pairCount;
+    int pairCount;
+    std::cin >> pairCount;
 
-	std::vector<std::vector<int>> adjacency;
-	adjacency.resize(static_cast<std::size_t>(nodeCount + 1));
+    std::vector<std::vector<int> > adjacency;
+    adjacency.resize(nodeCount + 1);
 
-	for (int i = 0; i < pairCount; ++i)
-	{
-		int u;
-		int v;
-		std::cin >> u >> v;
-		adjacency[static_cast<std::size_t>(u)].push_back(v);
-		adjacency[static_cast<std::size_t>(v)].push_back(u);
-	}
+    for (int i = 0; i < pairCount; ++i)
+    {
+        int u;
+        int v;
+        std::cin >> u >> v;
+        adjacency[u].push_back(v);
+        adjacency[v].push_back(u);
+    }
 
-	std::vector<char> visited;
-	visited.assign(static_cast<std::size_t>(nodeCount + 1), 0);
+    std::vector<char> visited;
+    visited.assign(nodeCount + 1, 0);
 
-	std::queue<int> bfsQueue;
-	visited[1] = 1;
-	bfsQueue.push(1);
+    std::queue<int> bfsQueue;
+    visited[1] = 1;
+    bfsQueue.push(1);
 
-	int infectedCount = 0;
+    int infectedCount = 0;
 
-	while (!bfsQueue.empty())
-	{
-		const int current = bfsQueue.front();
-		bfsQueue.pop();
+    while (!bfsQueue.empty())
+    {
+        int current = bfsQueue.front();
+        bfsQueue.pop();
 
-		for (std::size_t i = 0; i < adjacency[static_cast<std::size_t>(current)].size(); ++i)
-		{
-			const int next = adjacency[static_cast<std::size_t>(current)][i];
-			if (visited[static_cast<std::size_t>(next)] == 0)
-			{
-				visited[static_cast<std::size_t>(next)] = 1;
-				bfsQueue.push(next);
-				infectedCount = infectedCount + 1;
-			}
-		}
-	}
+        for (int i = 0; i < static_cast<int>(adjacency[current].size()); ++i)
+        {
+            int next = adjacency[current][i];
+            if (visited[next] == 0)
+            {
+                visited[next] = 1;
+                bfsQueue.push(next);
+                infectedCount = infectedCount + 1;
+            }
+        }
+    }
 
-	std::cout << infectedCount << std::endl;
-	return 0;
+    std::cout << infectedCount << std::endl;
+    return 0;
 }
