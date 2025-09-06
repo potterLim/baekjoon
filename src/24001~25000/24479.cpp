0a8b89424f904e0f675dc74d5b0e546798f88011
potterLim <potterLim0808@gmail.com>
2025-09-07 01:41:56 +0900

    [C++] 알고리즘 수업 - 깊이 우선 탐색 1

---
+++ src/24001~25000/24479.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <algorithm>
+#include <sstream>
+
+int main()
+{
+	int vertexCount;
+	int edgeCount;
+	int startVertex;
+	std::cin >> vertexCount >> edgeCount >> startVertex;
+
+	std::vector<std::vector<int>> adjacencyList(vertexCount + 1);
+
+	for (int i = 0; i < edgeCount; ++i)
+	{
+		int u;
+		int v;
+		std::cin >> u >> v;
+		adjacencyList[u].push_back(v);
+		adjacencyList[v].push_back(u);
+	}
+
+	for (int i = 1; i <= vertexCount; ++i)
+	{
+		std::sort(adjacencyList[i].begin(), adjacencyList[i].end());
+	}
+
+	std::vector<int> visitOrder(vertexCount + 1, 0);
+	std::vector<size_t> nextIndex(vertexCount + 1, 0);
+	std::stack<int> dfsStack;
+
+	int order = 1;
+	dfsStack.push(startVertex);
+
+	while (!dfsStack.empty())
+	{
+		int currentVertex = dfsStack.top();
+
+		if (visitOrder[currentVertex] == 0)
+		{
+			visitOrder[currentVertex] = order++;
+		}
+
+		bool pushed = false;
+		while (nextIndex[currentVertex] < adjacencyList[currentVertex].size())
+		{
+			int neighbor = adjacencyList[currentVertex][nextIndex[currentVertex]];
+			++nextIndex[currentVertex];
+
+			if (visitOrder[neighbor] == 0)
+			{
+				dfsStack.push(neighbor);
+				pushed = true;
+				break;
+			}
+		}
+
+		if (!pushed)
+		{
+			dfsStack.pop();
+		}
+	}
+
+	std::ostringstream out;
+	for (int i = 1; i <= vertexCount; ++i)
+	{
+		out << visitOrder[i] << '\n';
+	}
+	std::cout << out.str() << std::endl;
+
+	return 0;
+}
