2ef0151c83b7fdba926a4418ba7e390ca0622b46
Chunghyun Lim <potterLim0808@gmail.com>
2026-01-02 03:19:22 +0900

    [Java] 휴대폰 자판

--- src/5001~6000/5670.java
+++ src/5001~6000/5670.java
@@ -6,51 +6,39 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         StringBuilder output = new StringBuilder();
 
-
         while (scanner.hasNextInt()) {
             int wordCount = scanner.nextInt();
-
 
             String[] words = new String[wordCount];
             int totalLength = 0;
 
             for (int i = 0; i < wordCount; ++i) {
-                String word = scanner.next();
-
-                words[i] = word;
-                totalLength += word.length();
+                words[i] = scanner.next();
+                totalLength += words[i].length();
             }
 
             int maxNodes = totalLength + 2;
-            int rootIndex = rootIndex = 1;
-
+            int rootIndex = 1;
 
             int[][] nextNode = new int[26][maxNodes];
             int[] childCount = new int[maxNodes];
             boolean[] isTerminal = new boolean[maxNodes];
 
-
             int nodeCount = rootIndex;
 
-
             for (int i = 0; i < wordCount; ++i) {
-                String word = words[i];
                 int current = rootIndex;
+                String word = words[i];
 
                 for (int j = 0; j < word.length(); ++j) {
                     int letterIndex = word.charAt(j) - 'a';
-                    int child = nextNode[letterIndex][current];
 
-
-                    if (child == 0) {
-                        nodeCount++;
-                        child = nodeCount;
-
-                        nextNode[letterIndex][current] = child;
+                    if (nextNode[letterIndex][current] == 0) {
+                        nextNode[letterIndex][current] = ++nodeCount;
                         ++childCount[current];
                     }
 
-                    current = child;
+                    current = nextNode[letterIndex][current];
                 }
 
                 isTerminal[current] = true;
@@ -58,36 +46,28 @@ public class Main {
 
             long totalPresses = 0L;
 
-
             for (int i = 0; i < wordCount; ++i) {
-                String word = words[i];
-                int presses = 0;
                 int current = rootIndex;
+                int presses = 0;
+                String word = words[i];
 
                 for (int j = 0; j < word.length(); ++j) {
                     int letterIndex = word.charAt(j) - 'a';
-                    int child = nextNode[letterIndex][current];
 
-                    if (j == 0) {
+                    if (j == 0 || isTerminal[current] || childCount[current] > 1) {
                         ++presses;
-                    } else {
-                        if (isTerminal[current] || childCount[current] > 1) {
-                            ++presses;
-                        }
                     }
 
-                    current = child;
+                    current = nextNode[letterIndex][current];
                 }
 
                 totalPresses += presses;
             }
 
-            double average;
-
-            average = (double) totalPresses / (double) wordCount;
+            double average = (double) totalPresses / wordCount;
             output.append(String.format(Locale.US, "%.2f%n", average));
         }
 
-        System.out.print(output.toString());
+        System.out.print(output);
     }
 }
